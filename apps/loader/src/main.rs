91a026eaee78a09518e89d43cffe978a748c5004
Wenyu Huang <huangwenyuu@outlook.com>
2023-11-21 13:07:19 +0000

    exercise 1 & 2
    hello_app repo: https://github.com/uran0sH/hello_app

--- apps/loader/src/main.rs
+++ apps/loader/src/main.rs
@@ -8,18 +8,39 @@ const PLASH_START: usize = 0x22000000;
 
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
-    let apps_start = PLASH_START as *const u8;
-    let apps_size = 32; // Dangerous!!! We need to get accurate size of apps.
+    // let apps_start = (PLASH_START + 512) as *const u8;
+    let code = unsafe { core::slice::from_raw_parts(PLASH_START as *const u8, 1) };
+    let app_num = code[0];
+    println!("app_num={}", app_num);
+    let mut offset: usize = 1;
+    (0..app_num).for_each(|_i| {
+        let code = unsafe { core::slice::from_raw_parts((PLASH_START + offset) as *const u8, 2) };
+        let apps_size = ((code[0] as usize) << 8) + code[1] as usize; // Dangerous!!! We need to get accurate size of apps.
+        println!("apps_size={}",apps_size);
 
-    println!("Load payload ...");
+        println!("Load payload ...");
 
-    let code = unsafe { core::slice::from_raw_parts(apps_start, apps_size) };
-    println!("content: {:#x}", bytes_to_usize(&code[..8]));
-
-    println!("Load payload ok!");
+        let code = unsafe { core::slice::from_raw_parts((PLASH_START + 2 + offset) as *const u8, apps_size) };
+        println!("content: {:#x}", bytes_to_usize2(&code[..], apps_size));
+        offset += 2 + apps_size;
+        println!("Load payload ok!");
+    })
+    
 }
 
 #[inline]
 fn bytes_to_usize(bytes: &[u8]) -> usize {
     usize::from_be_bytes(bytes.try_into().unwrap())
+}
+
+#[inline]
+fn bytes_to_usize2(bytes: &[u8], size: usize) -> usize {
+    if size >= 8 {
+        return bytes_to_usize(bytes)
+    }
+    let mut result: usize = 0;
+    (0..size).for_each(|i| {
+        result = result << 8 | (bytes[i] as usize);
+    });
+    return result
 }
