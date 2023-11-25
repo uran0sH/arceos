8e7b755dc180617f69bbe0063ec9cdb597de47c7
Wenyu Huang <huangwenyuu@outlook.com>
2023-11-25 03:52:58 +0000

    exercise 4
    Signed-off-by: Wenyu Huang <huangwenyuu@outlook.com>

--- apps/loader/src/main.rs
+++ apps/loader/src/main.rs
@@ -4,9 +4,33 @@
 
 #[cfg(feature = "axstd")]
 use axstd::println;
+#[cfg(feature = "axstd")]
+use axstd::process::exit;
 
 const PLASH_START: usize = 0x22000000;
 const RUN_START: usize = 0xffff_ffc0_8010_0000;
+
+const SYS_HELLO: usize = 1;
+const SYS_PUTCHAR: usize = 2;
+const SYS_TERMINATE: usize = 3;
+
+static mut ABI_TABLE: [usize; 16] = [0; 16];
+
+fn register_abi(num: usize, handle: usize) {
+    unsafe { ABI_TABLE[num] = handle; }
+}
+
+fn abi_hello() {
+    println!("[ABI:Hello] Hello, Apps!");
+}
+
+fn abi_putchar(c: char) {
+    println!("[ABI:Print] {c}");
+}
+
+fn abi_terminate() {
+    exit(0);
+}
 
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
@@ -33,13 +57,31 @@ fn main() {
         offset += 2 + apps_size;
         println!("Load payload ok!");
 
+        register_abi(SYS_HELLO, abi_hello as usize);
+        register_abi(SYS_PUTCHAR, abi_putchar as usize);
+        register_abi(SYS_TERMINATE, abi_terminate as usize);
+    
         println!("Execute app ...");
+        let arg0: u8 = b'A';
+    
         // execute app
         unsafe { core::arch::asm!("
-            li t2, {run_start}
-            jalr t2",
+            li      t0, {abi_num}
+            slli    t0, t0, 3
+            la      t1, {abi_table}
+            add     t1, t1, t0
+            ld      t1, (t1)
+            jalr    t1
+            li      t2, {run_start}
+            jalr    t2
+            j       .",
             run_start = const RUN_START,
-        );}
+            abi_table = sym ABI_TABLE,
+            //abi_num = const SYS_HELLO,
+            abi_num = const SYS_TERMINATE,
+            in("a0") arg0,
+        )}
+    
     })
     
 }
