ca100025e4abf9e45f2753038c73ee2775adbeed
uran0sH <huangwenyuu@outlook.com>
2023-11-26 10:35:32 -0500

    step 5
    Signed-off-by: uran0sH <huangwenyuu@outlook.com>

--- apps/loader/src/main.rs
+++ apps/loader/src/main.rs
@@ -13,7 +13,9 @@ const SYS_PUTCHAR: usize = 2;
 static mut ABI_TABLE: [usize; 16] = [0; 16];
 
 fn register_abi(num: usize, handle: usize) {
-    unsafe { ABI_TABLE[num] = handle; }
+    unsafe {
+        ABI_TABLE[num] = handle;
+    }
 }
 
 fn abi_hello() {
@@ -32,16 +34,24 @@ fn main() {
     println!("Load payload ...");
 
     let load_code = unsafe { core::slice::from_raw_parts(load_start, load_size) };
-    println!("load code {:?}; address [{:?}]", load_code, load_code.as_ptr());
+    println!(
+        "load code {:?}; address [{:?}]",
+        load_code,
+        load_code.as_ptr()
+    );
 
+    unsafe {
+        init_app_page_table();
+    }
+    unsafe {
+        switch_app_aspace();
+    }
     // app running aspace
     // SBI(0x80000000) -> App <- Kernel(0x80200000)
     // 0xffff_ffc0_0000_0000
-    const RUN_START: usize = 0xffff_ffc0_8010_0000;
+    const RUN_START: usize = 0x4010_0000;
 
-    let run_code = unsafe {
-        core::slice::from_raw_parts_mut(RUN_START as *mut u8, load_size)
-    };
+    let run_code = unsafe { core::slice::from_raw_parts_mut(RUN_START as *mut u8, load_size) };
     run_code.copy_from_slice(load_code);
     println!("run code {:?}; address [{:?}]", run_code, run_code.as_ptr());
 
@@ -53,18 +63,46 @@ fn main() {
     println!("Execute app ...");
 
     // execute app
-    unsafe { core::arch::asm!("
+    unsafe {
+        core::arch::asm!("
         la      a7, {abi_table}
         li      t2, {run_start}
         jalr    t2
         j       .",
-        run_start = const RUN_START,
-        abi_table = sym ABI_TABLE,
-    )}
-
+            run_start = const RUN_START,
+            abi_table = sym ABI_TABLE,
+        )
+    }
 }
 
 #[inline]
 fn bytes_to_usize(bytes: &[u8]) -> usize {
     usize::from_be_bytes(bytes.try_into().unwrap())
+}
+
+//
+// App aspace
+//
+#[link_section = ".data.app_page_table"]
+static mut APP_PT_SV39: [u64; 512] = [0; 512];
+
+unsafe fn init_app_page_table() {
+    // 0x8000_0000..0xc000_0000, VRWX_GAD, 1G block
+    APP_PT_SV39[2] = (0x80000 << 10) | 0xef;
+    // 0xffff_ffc0_8000_0000..0xffff_ffc0_c000_0000, VRWX_GAD, 1G block
+    APP_PT_SV39[0x102] = (0x80000 << 10) | 0xef;
+
+    // 0x0000_0000..0x4000_0000, VRWX_GAD, 1G block
+    APP_PT_SV39[0] = (0x00000 << 10) | 0xef;
+
+    // For App aspace!
+    // 0x4000_0000..0x8000_0000, VRWX_GAD, 1G block
+    APP_PT_SV39[1] = (0x80000 << 10) | 0xef;
+}
+
+unsafe fn switch_app_aspace() {
+    use riscv::register::satp;
+    let page_table_root = APP_PT_SV39.as_ptr() as usize - axconfig::PHYS_VIRT_OFFSET;
+    satp::set(satp::Mode::Sv39, 0, page_table_root >> 12);
+    riscv::asm::sfence_vma_all();
 }
